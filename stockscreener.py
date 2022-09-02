c573f719d0f9205315206d2b4154820ec0b14ac5
taotto <67608103+taotto@users.noreply.github.com>
2022-09-02 16:08:01 +0200

    Update stockscreener.py

--- stockscreener.py
+++ stockscreener.py
@@ -4,6 +4,27 @@ import pandas as pd
 import numpy as np
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 import simfin as sf
+import mysql.connector
+
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
 
 sf.set_data_dir('~/simfin_data/')
 sf.set_api_key(api_key = st.secrets['key'])
